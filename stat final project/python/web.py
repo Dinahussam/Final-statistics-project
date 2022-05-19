edf7592576447c81e876dcbab09e9ce0d207331f
MariamTurky <93421790+MariamTurky@users.noreply.github.com>
2022-05-19 13:36:28 +0200

    Update web.py
    m

--- stat final project/python/web.py
+++ stat final project/python/web.py
@@ -2,32 +2,15 @@ from flask import Flask, redirect, url_for, request, render_template
 
 import pickle
 import numpy as np
-##def pre_process(data):
- ## data['waist_hip_ratio'] = data['waist_hip_ratio'].str.replace(',','.')
- ## data['waist_hip_ratio'] = data['waist_hip_ratio'].astype(float)
- ## data["chol_hdl_ratio"] = data["chol_hdl_ratio"].str.replace(",",".")
- ## data["chol_hdl_ratio"] = data["chol_hdl_ratio"].astype(float)
- ## data['bmi'] = data['bmi'].str.replace(',','.')
- ## data['bmi'] = data['bmi'].astype(float)
- ## data['gender'] = data['gender'].str.replace('female','1')
- ## data['gender'] = data['gender'].str.replace('male','0')
- ## data['gender'] = data['gender'].astype(int)
- ## data['diabetes'] = data['diabetes'].str.replace('Diabetes','1')
- ## data['diabetes'] = data['diabetes'].str.replace('No diabetes','0')
- ## data['diabetes'] = data['diabetes'].astype(int)
- ## data.drop(["patient_number"],axis=1,inplace=True)
- ## return data
-# def load_model(data):
-model = pickle.load(open('finalized_model.sav','rb'))
-  ##out = model.predict(np.array(data).reshape(1,-1))
-  ##return out[0]
+
+model = pickle.load(open('Diabetes.classifier.pickle','rb'))
 app = Flask(__name__, template_folder='templates')
-@app.route('/',methods=['GET'])
+@app.route('/')
 def home():
-    return render_template('prediction.html')
+    return render_template('home.html')
 
 
-##app.route('/predict', methods=['GET','POST'])
+app.route('/predict', methods=['POST'])
 def predict():
     Cholesterol = request.form['a']
     Glucose = request.form['b']
@@ -43,13 +26,17 @@ def predict():
     waist = request.form['l']
     hip = request.form['m']
     waist_hip_ratio = request.form['n']
+    if (gender=="Male"):
+        gender=1
+    else:
+        gender =0
 
     Cholesterol = int(Cholesterol)
     Glucose = int(Glucose)
     hdl_chol = int(hdl_chol)
     chol_hdl_ratio = float(chol_hdl_ratio)
     age = int(age)
-    gender = int(gender)
+    gender =(gender)
     hight = int(hight)
     weight = int(weight)
     bmi = float(bmi)
@@ -63,28 +50,7 @@ def predict():
                           diastolic_bp, waist, hip, waist_hip_ratio)])
     prediction = model.predict(features)
     prediction = 'Diabetic' if prediction == 0 else "Not Diabetic"
-    return render_template('index.html', prediction_text=' The pationt has diabetes:{}'.format(prediction))
- ##if request == 'POST':
- ##  f = request.file['file']
- ##  data = pre_process(f)
- ##  out = load_model(data)
-    ## if out == 0
-    ##    return 'no diabetes'
-    ## else
-     ##   return 'diabetes'
- ##  return out
- ##return none
-if __name__ == 'main':
+    return render_template('prediction.html', prediction_text=' The pationt has diabetes:{}'.format(prediction))
+
+if __name__ == '__main__':
     app.run(debug=True, port=7000)
-
-
-#########
-#To use the predict button in our web-app
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     #For rendering results on HTML GUI
-#     int_features = [float(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
-#     prediction = model.predict(final_features)
-#     output = round(prediction[0], 2)
-#     return render_template('index.html', prediction_text='CO2    Emission of the vehicle is :{}'.format(output))
